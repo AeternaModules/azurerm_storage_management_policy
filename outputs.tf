52e0342215cec4d1131e0b3afd50d60e8f11c589
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:02 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "storage_management_policies_id" {
+  description = "Map of id values across all storage_management_policies, keyed the same as var.storage_management_policies"
+  value       = { for k, v in azurerm_storage_management_policy.storage_management_policies : k => v.id }
+}
 output "storage_management_policies_rule" {
   description = "Map of rule values across all storage_management_policies, keyed the same as var.storage_management_policies"
   value       = { for k, v in azurerm_storage_management_policy.storage_management_policies : k => v.rule }
